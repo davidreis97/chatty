10ff66e2c5c1820b8c67723c7900d80ba45cde78
davidreis97 <david.alex.reis.97@gmail.com>
2021-11-28 20:15:20 +0000

    Add frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Badge } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Badge>Default</Badge>
+    </div>
+  )
+}
+
+export default Home
